61cfa05b111c21947c57fe465c7f8d4899067a70
Anton Nazarenko <antoinenaza@gmail.com>
2023-07-14 12:27:12 +0600

    docs: update gif

--- src/task/task.service.spec.ts
+++ src/task/task.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskService } from './task.service';
+import { queueProvider } from '../call-queue/call-queue.module';
+import { telegramProvider } from '../telega/telega.module';
+import { ConfigModule } from '@nestjs/config';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import { LevelEngService } from '../level-eng/level-eng.service';
+import { CALL_QUEUE, TELEGRAM } from '../constants';
+import { HttpModule } from '@nestjs/axios';
 
 describe('TaskService', () => {
   let service: TaskService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
+      imports: [HttpModule, ConfigModule],
+      providers: [
+        {
+          provide: TELEGRAM,
+          useValue: telegramProvider,
+        },
+        {
+          provide: CALL_QUEUE,
+          useValue: queueProvider,
+        },
+        TaskService,
+        SchedulerRegistry,
+        LevelEngService,
+      ],
     }).compile();
 
     service = module.get<TaskService>(TaskService);
